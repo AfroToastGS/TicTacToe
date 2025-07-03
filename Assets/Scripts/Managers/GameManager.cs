4a9d4a90695d425b4ac08fc1385396f8f1699a13
Fadi Abuzant <fadi.abuzant@afrotoast.com>
2025-07-03 21:58:20 +0300

    - Add MainMenu scene - Add Game scene - Add GameManager and switch scenes logic

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+
+namespace TicTacToe
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        #region Inspector properties
+
+        #endregion
+
+        #region Public properties
+        // Singleton instance
+        public static GameManager Instance { get; private set; }
+
+        public Action<EnumScenes> OnSceneChangeRequested;
+
+        #endregion
+
+        #region Private properties
+
+        #endregion
+
+        #region Behaviours
+
+        private void Awake()
+        {
+            // Singleton pattern implementation
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject); // Keep this object alive when changing scenes
+            }
+            else
+            {
+                Destroy(gameObject); // Destroy duplicate instances
+            }
+        }
+
+        private void Start()
+        {
+            // Subscribe to the scene change event
+            OnSceneChangeRequested += OnSceneChangeRequestedHandler;
+        }
+
+
+        private void Update()
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+            // Unsubscribe from the event to prevent memory leaks
+            OnSceneChangeRequested -= OnSceneChangeRequestedHandler;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnSceneChangeRequestedHandler(EnumScenes scenes)
+        {
+            // Load the requested scene
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scenes.ToSceneName());
+        }
+
+        #endregion
+    }
+}
